f5f72b9178f7ba09eea5bc3ea5fa7c28cbeb91e5
Erik Craddock <erik@craddock.org>
2023-10-19 12:13:50 -0500

    reverts site value which is required for rss

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,6 @@ import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 
 export default defineConfig({
+  site: 'https://erikvancraddock.com',
   integrations: [mdx(), sitemap(), tailwind(), react()]
 });
